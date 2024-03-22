fe222f797c54ff5fc2b399e0e74735c1baae9693
prajwoldangol <prajwoldangol@gmail.com>
2024-03-22 13:36:29 -0700

    PA 303.7.1 - Task 2

--- src/com/pa/arrays/MiddleElement.java
+++ src/com/pa/arrays/MiddleElement.java
@@ -1,2 +1,11 @@
-package com.pa.arrays;public class MiddleElement {
+package com.pa.arrays;
+
+public class MiddleElement {
+    public static void main(String[] args) {
+        //Task 2: Write a program that returns the middle element in an array. Give the following
+        //values to the integer array: {13, 5, 7, 68, 2} and produce the following output: 7
+        int[] arr = {13, 5, 7, 68, 2} ;
+        int midIndex = arr.length/2 ;
+        System.out.println(arr[midIndex]);
+    }
 }
