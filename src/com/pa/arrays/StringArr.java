4e8a4f2d31ee5eecae40a67e459194ba1bb43a74
prajwoldangol <prajwoldangol@gmail.com>
2024-03-22 13:38:03 -0700

    PA 303.7.1 - Task 3

--- src/com/pa/arrays/StringArr.java
+++ src/com/pa/arrays/StringArr.java
@@ -1,2 +1,16 @@
-package com.pa.arrays;public class StringArr {
+package com.pa.arrays;
+
+import java.util.Arrays;
+
+public class StringArr {
+    public static void main(String[] args) {
+        //Task 3: Write a program that creates an array of String type and initializes it with the
+        //strings “red,” “green,” “blue,” and “yellow.” Print out the array length. Make a copy
+        //using the clone( ) method. Use the Arrays.toString( ) method on the new array to
+        //verify that the original array was copied.
+        String[] colors = {"Red","Green","Yellow","Blue"};
+        System.out.println(colors.length);
+        String[] clone = colors.clone();
+        System.out.println(Arrays.toString(clone));
+    }
 }
