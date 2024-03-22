c8465b0b618b9d9c666aa786a6083bcc8bc06166
prajwoldangol <prajwoldangol@gmail.com>
2024-03-22 13:49:35 -0700

    PA.303.7.1 -Task 7

--- src/com/pa/arrays/ArrayLoopThruThree.java
+++ src/com/pa/arrays/ArrayLoopThruThree.java
@@ -1,2 +1,19 @@
-package com.pa.arrays;public class ArrayLoopThruThree {
+package com.pa.arrays;
+
+import java.util.Arrays;
+
+public class ArrayLoopThruThree {
+    public static void main(String[] args) {
+        //Task 7: Write a program where you create an array of 5 elements. Loop through the
+        //array and print the value of each element, except for the middle (index 2) element.
+        int[] nums = {5,4,8,9,11};
+        int mid = nums.length / 2 ;
+        for(int i =0; i<nums.length; i++){
+            if(i != mid ){
+                System.out.println(nums[i]);
+            }
+
+        }
+
+    }
 }
