a22ff8f3de0b400d389d6d199398fb298839337f
prajwoldangol <prajwoldangol@gmail.com>
2024-03-22 13:48:22 -0700

    PA 303.7.1 - Task 5

--- src/com/pa/arrays/ArrayLoopThru.java
+++ src/com/pa/arrays/ArrayLoopThru.java
@@ -1,2 +1,16 @@
-package com.pa.arrays;public class ArrayLoopThru {
+package com.pa.arrays;
+
+import java.util.Arrays;
+
+public class ArrayLoopThru {
+    public static void main(String[] args) {
+        //Task 5: Write a program where you create an integer array with a length of 5. Loop
+        //through the array and assign the value of the loop control variable (e.g., i) to the
+        //corresponding index in the array
+        int[] nums = new int[5];
+        for(int i =0; i<nums.length; i++){
+            nums[i] = i ;
+        }
+        System.out.println(Arrays.toString(nums));
+    }
 }
